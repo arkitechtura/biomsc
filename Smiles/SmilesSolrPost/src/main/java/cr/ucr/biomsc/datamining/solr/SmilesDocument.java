ab5694918c5263d2a465815d26d0b4822d5a48e9
equiros <equiros@arkitechtura.com>
2014-05-15 16:27:07 -0600

    UI change for query string

---
+++ Smiles/SmilesSolrPost/src/main/java/cr/ucr/biomsc/datamining/solr/SmilesDocument.java
@@ -0,0 +1,7 @@
+package cr.ucr.biomsc.datamining.solr;
+
+/**
+ * Created by equiros on 5/14/2014.
+ */
+public class SmilesDocument {
+}
---
+++ Smiles/SolrSmiles/src/main/java/cr/ucr/biomsc/datamining/solr/SmilesUtilities.java
@@ -0,0 +1,7 @@
+package cr.ucr.biomsc.datamining.solr;
+
+/**
+ * Created by equiros on 5/13/2014.
+ */
+public class SmilesUtilities {
+}
